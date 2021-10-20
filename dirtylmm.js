6af499061ce340a851c1e36c705f92df3d5d3910
alvaro <a.garciatorr@gmail.com>
2021-10-21 00:05:28 +0200

    Added lmm exercises

---
+++ dirtylmm.js
@@ -0,0 +1,90 @@
+const binary = "91 3f 91 1f 92 00 00 00 00 00 00 00 00 00 00 00";
+const data = 8;
+const addresses = 4;
+const oc = 4;
+
+const dict = {
+    "0000": "END",
+    "0001": "ADD",
+    "0010": "SUBTRACT",
+    "0011": "STORE",
+    "0101": "LOAD",
+    "0110": "BRANCH",
+    "0111": "BRANCH_EQ_0",
+    "1000": "BRANCH_GE_0",
+    "1001": {
+        "0001": "INPUT",
+        "0010": "OUTPUT"
+    }
+}
+
+const withoutOpers = ["END", "INPUT", "OUTPUT"]
+
+/*const zeros = Array(data).fill(0).join("");
+const zeros_addresses = Array(addresses).fill(0).join("");*/
+
+const toBinary = (x) => x.toString(2);
+const addZeros = function(x, bits) {
+    const zeros = Array(bits).fill(0).join("")
+    x + zeros;
+} 
+const trimZeros = (x, bits) => x.slice(-bits);
+
+simplificado = compose(addZeros(data), toBinary(2));
+
+console.log(simplificado);
+
+/*const memory = Array(2**addresses).fill(0)
+        .map((_,i) => i)
+        .map(x => x.toString(2))
+        .map(x => zeros_addresses + x)
+        .map(x => x.slice(-addresses))
+        ;
+
+console.log(memory);*/
+
+/*
+const ram = binary.split(" ")
+        .map(x => parseInt(x, 16))
+        .map(x => x.toString(2))
+        .map(x => zeros + x)
+        .map(x => x.slice(-data))
+        .map((x, i) => (
+            {
+                address: memory[i],
+                oc: x.slice(0,oc),
+                oper: x.slice(oc)
+            }
+        ))
+        ;
+
+function printRAM(ram){
+    console.log("RAM");
+    console.log("Address\t\tValue");
+    ram.forEach(({address, oc, oper}) => console.log(`${address} \t\t${oc}${oper}`));
+}
+
+printRAM(ram);
+
+function decompile(ram){
+    console.log("SOURCE");
+    console.log("Address\t\tInstruction");
+
+    for(let i=0; i<ram.length; i++){
+        let {address, oc, oper} = ram[i];
+
+        let nemonic = dict[oc];
+        if(typeof nemonic === "object"){
+            nemonic = nemonic[oper];
+        }
+
+        if(withoutOpers.includes(nemonic)){
+            oper = "";
+        } else {
+            oper = " " + oper;
+        }
+        console.log(`${address} \t\t${nemonic}${oper}`);
+    }
+}
+console.log();
+decompile(ram);*/
---
+++ inverselmm.js
@@ -0,0 +1,79 @@
+const data = 8;
+const addresses = 4;
+const oc = 4;
+
+const program_output =[    
+    "INPUT",
+    "STORE 1111",
+    "INPUT",
+    "ADD 1111",
+    "OUTPUT"
+] 
+
+const dict = {
+    "0000": "END",
+    "0001": "ADD",
+    "0010": "SUBTRACT",
+    "0011": "STORE",
+    "0101": "LOAD",
+    "0110": "BRANCH",
+    "0111": "BRANCH_EQ_0",
+    "1000": "BRANCH_GE_0",
+    "1001": {
+        "0001": "INPUT",
+        "0010": "OUTPUT"
+    }
+}
+
+const withoutOpers = ["END", "INPUT", "OUTPUT"]
+
+const zeros = Array(data).fill(0).join("");
+const zeros_addresses = Array(addresses).fill(0).join("");
+
+
+function getKeyByValue(object, value){
+    return Object.keys(object).find(key => object[key] === value)
+}
+
+function getAllOper(program_output){
+    for(let i=0; i<program_output.length; i++){
+    if(withoutOpers.includes(program_output[i])){
+        if(program_output[i] === "INPUT"){
+            program_output[i] += " 0001";
+        } else if (program_output[i] === "OUTPUT"){
+            program_output[i] += " 0010";
+        } else if (program_output[i] === "END") program_output[i] += " 0000";
+    }
+    
+    }
+}
+
+getAllOper(program_output);
+
+const instructions = program_output.map((x, i) => (
+    {        
+        oper: x.slice(-4),
+        oc: x.slice(0, -5)
+    } 
+    ));
+
+const getBinaryOc = function(instructions){
+    for(let i=0; i<instructions.length; i++){
+        if(instructions[i].oc === "INPUT" || instructions[i].oc === "OUTPUT"){
+            instructions[i].oc = "1001";
+        } else instructions[i].oc = getKeyByValue(dict, instructions[i].oc);
+        console.log(instructions[i]);
+    }
+}
+
+getBinaryOc(instructions)
+
+ram = instructions.map((x, i) =>(
+                        instructions[i].oc + instructions[i].oper
+                        ))
+                        .map(x => parseInt(x, 2))
+                        .map(x => x.toString(16)).join(" ");
+
+
+console.log();
+console.log(ram);
